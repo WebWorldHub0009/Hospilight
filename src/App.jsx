a264c44c3c5fb4fc0515b90d8f4911ab1469e4bb
AbdulWaris009 <abdulwariidrisi11@gmail.com>
2025-07-16 12:40:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+export default App;
